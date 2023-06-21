59a877dd4f7ea8470d3761f591808326ef90fd01
Samuel Raducan <samuel.thembp@gmail.com>
2023-06-21 20:47:56 +0100

    Client setup

--- server/server.ts
+++ server/server.ts
@@ -3,5 +3,5 @@ config();
 import fastify from "fastify";
 
 const app = fastify();
-
+console.log("Server is running on port " + process.env.PORT!);
 app.listen({ port: parseInt(process.env.PORT!) });
