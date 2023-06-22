859690d07840d83bcf2882d2639d056f5f10759f
Samuel Raducan <samuel.thembp@gmail.com>
2023-06-22 21:02:26 +0100

    Add the remaining input fields and the button for the sign up page

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,21 @@
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  forwardRef,
+} from "react";
+
+export const Button = forwardRef<
+  HTMLButtonElement,
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <button
+      className={`border-2 border-gray-900 bg-blue-600 rounded p-2 w-full text-white font-bold hover:bg-blue-500 focus:bg-blue-400 tranition-colors disabled:bg-gray-500 ${className}`}
+      {...rest}
+      ref={ref}
+    >
+      {children}
+    </button>
+  );
+});
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,6 +1,12 @@
+import { useRef } from "react";
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function Signup() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const imageUrlRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <h1 className="text-3xl font-bold mb-8 text-center">Sign up</h1>
@@ -8,8 +14,18 @@ export function Signup() {
         className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 
             items-center justify-items-end"
       >
-        <label>Username</label>
-        <Input />
+        <label htmlFor="username">Username</label>
+        <Input id="username" pattern="\S*" required ref={usernameRef} />
+
+        <label htmlFor="name">Name</label>
+        <Input id="name" required ref={nameRef} />
+
+        <label htmlFor="imageURL">Image URL</label>
+        <Input id="imageURL" type="url" pattern="\S*" ref={imageUrlRef} />
+
+        <Button type="submit" className="col-span-full">
+          Signg Up
+        </Button>
       </form>
     </>
   );
