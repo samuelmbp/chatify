99e4afe6b656915dff732d6e99bcc2c5810b9372
Samuel Raducan <samuel.thembp@gmail.com>
2023-06-22 20:45:45 +0100

    Implement the input component for the sign up page

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,3 +1,16 @@
+import { Input } from "../components/Input";
+
 export function Signup() {
-  return <h1>Signup page</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-8 text-center">Sign up</h1>
+      <form
+        className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 
+            items-center justify-items-end"
+      >
+        <label>Username</label>
+        <Input />
+      </form>
+    </>
+  );
 }
