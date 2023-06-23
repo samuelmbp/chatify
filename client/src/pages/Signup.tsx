dffa907737ed63f09dbc787b929d06dd96da7d71
Samuel Raducan <samuel.thembp@gmail.com>
2023-06-23 11:17:16 +0100

    Create the client server connection to register a new user

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,16 +1,33 @@
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useAuth } from "../context/AuthContext";
 
 export function Signup() {
+  const { signup } = useAuth();
   const usernameRef = useRef<HTMLInputElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
   const imageUrlRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (signup.isLoading) return;
+
+    const username = usernameRef.current?.value;
+    const name = nameRef.current?.value;
+    const imageURL = imageUrlRef.current?.value;
+
+    if (username === null || username === "" || name === null || name === "")
+      return;
+
+    signup.mutate({ id: username, name, image: imageURL });
+  };
 
   return (
     <>
       <h1 className="text-3xl font-bold mb-8 text-center">Sign up</h1>
       <form
+        onSubmit={handleSubmit}
         className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 
             items-center justify-items-end"
       >
@@ -23,8 +40,12 @@ export function Signup() {
         <label htmlFor="imageURL">Image URL</label>
         <Input id="imageURL" type="url" pattern="\S*" ref={imageUrlRef} />
 
-        <Button type="submit" className="col-span-full">
-          Signg Up
+        <Button
+          disabled={signup.isLoading}
+          type="submit"
+          className="col-span-full"
+        >
+          {signup.isLoading ? "Loading..." : "Signg Up"}
         </Button>
       </form>
     </>
